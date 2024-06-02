6e6a5f28cc17bcc6513e895e46b0eff1cc6e3fed
vinay878787 <vinay878787@gmail.com>
2024-06-02 16:22:42 +0530

    everything is ready except invite container

--- frontend/src/pages/quickmatch/QuickMatch.jsx
+++ frontend/src/pages/quickmatch/QuickMatch.jsx
@@ -5,21 +5,35 @@ import axios from "axios";
 
 function QuickMatch() {
   const [battleTypes, setBattleTypes] = useState([]);
+  const [battleName, setBattleName] = useState("");
+  const [battleData, setBattleData] = useState({});
 
   const fetchBattleData = async () => {
-    const battleData = await axios.get("http://localhost:3000/quickmatch");
-    setBattleTypes(battleData.data.message);
+    const response = await axios.get(
+      "https://battlefield-dhmr.onrender.com/quickmatch"
+    );
+    setBattleTypes(response.data.message);
   };
+
   useEffect(() => {
     fetchBattleData();
-  }, [battleTypes]);
+  }, []);
+
+  const handleMouseEnter = (name) => {
+    setBattleName(name);
+    const foundBattle = battleTypes.find((battle) => battle.title === name);
+    if (foundBattle) {
+      setBattleData(foundBattle);
+    }
+  };
+
   return (
     <div className={styles.quickMatchContainer}>
       <div className={styles.quickMatchSubContainer1}>
         <div className={styles.battleFieldIcons}>
           <NavLink to="/">
             <img
-              src="/battleField1Img.png"
+              src="/battleField5Img.png"
               className={styles.navIcons}
               alt="battleField1Img"
               title="BATTLEFIELD V"
@@ -27,7 +41,7 @@ function QuickMatch() {
           </NavLink>
           <NavLink to="/">
             <img
-              src="/battleField2Img.png"
+              src="/battleField4.png"
               className={styles.navIcons}
               alt="battleField2Img"
               title="BATTLEFIELD 1"
@@ -43,7 +57,7 @@ function QuickMatch() {
           </NavLink>
           <NavLink to="/">
             <img
-              src="/battleField4.png"
+              src="/battleField2Img.png"
               className={styles.navIcons}
               alt="battleField4Img"
               title="BATTLEFIELD HARDLINE"
@@ -51,7 +65,7 @@ function QuickMatch() {
           </NavLink>
           <NavLink to="/">
             <img
-              src="/battleField5Img.png"
+              src="/battleField1Img.png"
               className={styles.navIcons}
               alt="WATCH"
               title="CAREER"
@@ -98,25 +112,48 @@ function QuickMatch() {
       <div className={styles.quickMatchSubContainer2}>
         <div className={styles.heading}>
           <div className={styles.headingSubContainer}>
-          <img
-            src="https://uploads-ssl.webflow.com/6013fff62154adaa4600f932/601ab21dde67c22de2b6d61b_back.svg"
-            className={styles.arrow}
-          ></img>
-          <div className={styles.heading1}>MULTIPLAYER /</div>
+            <img
+              src="https://uploads-ssl.webflow.com/6013fff62154adaa4600f932/601ab21dde67c22de2b6d61b_back.svg"
+              className={styles.arrow}
+            ></img>
+            <div className={styles.heading1}>MULTIPLAYER /</div>
           </div>
 
           <div className={styles.heading2}>QUICKMATCH</div>
         </div>
         <div className={styles.battleTypes}>
           {battleTypes.map((battleItem) => (
-            <ul key={battleItem.id} className={styles.battleItem}>
+            <ul
+              key={battleItem.id}
+              className={styles.battleItem}
+              onMouseEnter={() => handleMouseEnter(battleItem.title)}
+            >
               <li className={styles.battleTitle}>{battleItem.title}</li>
             </ul>
           ))}
         </div>
       </div>
-      <div className={styles.quickMatchSubContainer3}>3</div>
-      <div className={styles.quickMatchSubContainer4}>4</div>
+      <div className={styles.quickMatchSubContainer3}>
+        <div className={styles.dataRepresentation}>
+          {battleData && (
+            <div>
+              <img
+                src={battleData.imageUrl}
+                className={styles.battleImageUrl}
+              ></img>
+              <h3 className={styles.battleDataTitle}>{battleData.title}</h3>
+              <p className={styles.battleDescription}>
+                {battleData.description}
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className={styles.quickMatchSubContainer4}>
+      <div className={styles.inviteContainer}>
+      <img src="/invite.png" className={styles.inviteImg}></img>
+      </div>
+      </div>
     </div>
   );
 }
